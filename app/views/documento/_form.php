9eac6b7014d1ac5456e7633c9c7abee92cce6921
HenryVegaAyala <hp.vega@hotmail.com>
2017-01-29 09:11:40 -0500

    sysventas

--- app/views/documento/_form.php
+++ app/views/documento/_form.php
@@ -2,40 +2,57 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\file\FileInput;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Documento */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="documento-form">
+<div class="panel panel-default">
+
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= $this->title ?></h3>
+    </div>
+
+    <div class="container-fluid">
+        <p class="note"></p>
+    </div>
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'Codigo_Documento')->textInput() ?>
-
-    <?= $form->field($model, 'Nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'archivo')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'Fecha_Creado')->textInput() ?>
-
-    <?= $form->field($model, 'Fecha_Modificado')->textInput() ?>
-
-    <?= $form->field($model, 'Fecha_Eliminado')->textInput() ?>
-
-    <?= $form->field($model, 'Usuario_Creado')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'Usuario_Eliminado')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'Usuario_Modificado')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'Estado')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    <div class="fieldset">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <?= $form->field($model, 'Nombre')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->field($model, 'archivo')->textInput(['maxlength' => true]) ?>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php
 
+    echo $form->field($model, 'archivo')->widget(FileInput::classname(), [
+        'options' => ['accept' => 'image/*'],
+    ]);
+
+    echo '<label class="control-label">Upload Document</label>';
+    echo FileInput::widget([
+        'name' => 'archivo',
+    ]);
+
+    ?>
+
+    <div class="panel-footer container-fluid foo">
+        <div class="col-sm-12">
+            <?= Html::submitButton($model->isNewRecord ? "<i class=\"fa fa-plus-square\" aria-hidden=\"true\"></i> Guardar" : 'Update', ['class' => $model->isNewRecord ? 'btn btn-primary ' : 'btn btn-primary ']) ?>
+            <?= Html::resetButton($model->isNewRecord ? "<i class=\"fa fa-eraser\" aria-hidden=\"true\"></i> Limpiar" : 'Update', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
+            <?= Html::a("<i class=\"fa fa-chevron-circle-left\" aria-hidden=\"true\"></i> Cancelar", ['index'], ['class' => 'btn btn-primary']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
--- app/views/documento/create.php
+++ app/views/documento/create.php
@@ -6,13 +6,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Documento */
 
-$this->title = 'Create Documento';
-$this->params['breadcrumbs'][] = ['label' => 'Documentos', 'url' => ['index']];
+$this->title = 'Registrar Documento';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="documento-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,

ffa64f4cc44c88e31a86c4415d4de953ef518385
Henry <hvega@dtransforma.com>
2017-02-23 17:41:18 -0500

    sysventas

--- views/venta/reporteventa.php
+++ views/venta/reporteventa.php
@@ -90,79 +90,82 @@ class PDF extends FPDF
 
         $connection = \Yii::$app->db;
         $sqlStatement = '
-         SELECT DISTINCT
-          numero_contrato AS NUMEROCONTRATO,
-          serie_comprobante AS SERIE,
-          numero_comprobante AS COMPROBANTE,
-          c.Nombre AS NOMBRE,
-          c.Apellido AS APELLIDO,
-          CASE
-          c.Estado_Civil
-          WHEN 0
-            THEN "S"
-          WHEN 1
-            THEN "C"
-          WHEN 2
-            THEN "CO"
-          WHEN 3
-            THEN "D"
-          WHEN 4
-            THEN "V"
-          END AS Estado_Civil,
-          c.Profesion AS PROFESION,
-          c.DNI AS DNI,
-          c.Edad AS EDAD,
-          c.Telefono_Celular AS TELEFONOCELULAR,
-          c.Email AS EMAIL,
-          cl.Nombre AS NOMBRECLUB,
-          p.Nombre AS NOMBREPASPORTE,
-          v.numero_pasaporte AS NUMEROPASAPORTE,
-          CASE
-          cl.Vigencia
-          WHEN 1
-            THEN "10"
-          WHEN 2
-            THEN "20"
-          WHEN 3
-            THEN "30"
-          END AS VigenciaDias,
-          co.convetidor1 AS CONVER1,
-          co.convetidor2 AS CONVER2,
-          co.Regalos AS REGALOS,
-          co.Observacion AS OBSER,
-          CASE
-          pa.tipo_pago
-          WHEN 0
-            THEN "Tarjeta de Credito"
-          WHEN 1
-            THEN "Tarjeta de Debito"
-          WHEN 2
-            THEN "Plazo"
-          WHEN 3
-            THEN "Letras"
-          WHEN 4
-            THEN "Cash"
-          END AS tipo_pago,
-          pa.monto_pagado AS MONTO,
-          pa.monto_ingresado AS INGRESA,
-          pa.monto_restante AS RESTANTE,
-          fpa.fecha_pago AS FECHAPAGO,
-          CASE
-          pa.estado_pago
-          WHEN 0
-            THEN "Adelanto"
-          WHEN 1
-            THEN "Pendiente"
-          WHEN 2
-            THEN "Cancelado"
-          END AS estado_pago
-        FROM venta v
-          INNER JOIN cliente c ON v.Codigo_Cliente = c.Codigo_Cliente
-          INNER JOIN club cl ON v.Codigo_club = cl.Codigo_club
-          INNER JOIN pasaporte p ON v.Codigo_pasaporte = v.Codigo_pasaporte
-          INNER JOIN combo co ON v.Codigo_venta = co.Codigo_venta
-          INNER JOIN pago pa ON v.Codigo_venta = pa.Codigo_venta
-          INNER JOIN formas_pago fpa ON pa.codigo_pago = fpa.codigo_pago
+            SELECT DISTINCT
+              numero_contrato    AS NUMEROCONTRATO,
+              serie_comprobante  AS SERIE,
+              numero_comprobante AS COMPROBANTE,
+              c.Nombre           AS NOMBRE,
+              c.Apellido         AS APELLIDO,
+              CASE
+              c.Estado_Civil
+              WHEN 0
+                THEN "S"
+              WHEN 1
+                THEN "C"
+              WHEN 2
+                THEN "CO"
+              WHEN 3
+                THEN "D"
+              WHEN 4
+                THEN "V"
+              END                AS Estado_Civil,
+              c.Profesion        AS PROFESION,
+              c.DNI              AS DNI,
+              c.Edad             AS EDAD,
+              c.Telefono_Celular AS TELEFONOCELULAR,
+              c.Email            AS EMAIL,
+              cl.Nombre          AS NOMBRECLUB,
+              p.Nombre           AS NOMBREPASPORTE,
+              v.numero_pasaporte AS NUMEROPASAPORTE,
+              CASE
+              cl.Vigencia
+              WHEN 1
+                THEN "10"
+              WHEN 2
+                THEN "20"
+              WHEN 3
+                THEN "30"
+              END                AS VigenciaDias,
+              co.convetidor1     AS CONVER1,
+              co.convetidor2     AS CONVER2,
+              co.Regalos         AS REGALOS,
+              co.Observacion     AS OBSER,
+              CASE
+              pa.tipo_pago
+              WHEN 0
+                THEN "Tarjeta de Credito"
+              WHEN 1
+                THEN "Tarjeta de Debito"
+              WHEN 2
+                THEN "Plazo"
+              WHEN 3
+                THEN "Letras"
+              WHEN 4
+                THEN "Cash"
+              END                AS tipo_pago,
+              pa.monto_pagado    AS MONTO,
+              pa.monto_ingresado AS INGRESA,
+              pa.monto_restante  AS RESTANTE,
+              fpa.fecha_pago     AS FECHAPAGO,
+              CASE
+              pa.estado_pago
+              WHEN 0
+                THEN "Adelanto"
+              WHEN 1
+                THEN "Pendiente"
+              WHEN 2
+                THEN "Cancelado"
+              END                AS estado_pago,
+              CONCAT(ben.Nombre,\' \',ben.Apellido) as Beneficiario
+            FROM venta v
+              INNER JOIN cliente c ON v.Codigo_Cliente = c.Codigo_Cliente
+              INNER JOIN club cl ON v.Codigo_club = cl.Codigo_club
+              INNER JOIN pasaporte p ON v.Codigo_pasaporte = v.Codigo_pasaporte
+              INNER JOIN combo co ON v.Codigo_venta = co.Codigo_venta
+              INNER JOIN pago pa ON v.Codigo_venta = pa.Codigo_venta
+              INNER JOIN formas_pago fpa ON pa.codigo_pago = fpa.codigo_pago
+              INNER JOIN comision com ON v.Codigo_venta = com.Codigo_venta
+            INNER JOIN beneficiario ben ON c.Codigo_Cliente = ben.Codigo_Cliente
         WHERE date(v.Fecha_Creado)  BETWEEN "' . $fechaIni . '" and "' . $fechaFin . '";
         ';
         $comando = $connection->createCommand($sqlStatement);
@@ -189,7 +192,7 @@ class PDF extends FPDF
             $this->Cell(2.5, 0.7, utf8_decode(strtoupper($row['VigenciaDias'])), 1, '', 'C');
             $this->Cell(2, 0.7, utf8_decode(strtoupper($row['VigenciaDias'])), 1, '', 'C');
 
-            $this->Cell(3.5, 0.7, utf8_decode(strtoupper('')), 1, '', 'C');
+            $this->Cell(3.5, 0.7, utf8_decode(strtoupper($row['Beneficiario'])), 1, '', 'C');
             $this->Cell(2, 0.7, utf8_decode(strtoupper('')), 1, '', 'C');
 
             $this->Cell(4, 0.7, utf8_decode(strtoupper($row['CONVER1'])), 1, '', 'C');
@@ -208,60 +211,60 @@ class PDF extends FPDF
 
     }
 
-    function Comision($fechaIni, $fechaFin)
-    {
-        $model = new \app\models\Usuario();
-        $cliente = new \app\models\Cliente();
-
-//      $this->Cell(Ancho , Alto , cadena , bordes , posición , alinear , fondo, URL )
-        $this->SetFont('Arial', 'B', 18);
-
-//      $this->Image('ruta de imagen', horizontal, vertical, ancho, alto);
-//        $this->Image(Yii::getAlias('@groupgygUrlReporte'), 1, 1, 1.5, 1.5);
-        $this->Cell(78.5, 1, utf8_decode('Lista de Comisiones'), 0, 'C', 'C');
-        $this->Ln(2);
-
-        $this->SetFont('Arial', 'B', 11);
-        $this->SetFillColor(150, 54, 52); // establece el color del fondo de la celda.
-        $this->SetDrawColor(150, 54, 52);     // establece el color del contorno de la celda.
-        $this->SetTextColor(255, 255, 255);  // Establece el color del texto.
-        $this->Cell(0.2, 0.2, utf8_decode(''), 0, 0, 'C', True);
-        $this->Cell(75.1, 0.2, utf8_decode(''), 1, 0, 'C', True);
-        $this->Cell(0.2, 0.2, utf8_decode(''), 0, 0, 'C', True);
-        $this->Ln(0.6);
-
-
-        $this->Cell(5, 0.7, utf8_decode(strtoupper('Datos del Usuario')), 1, '', 'C');
-        $this->Cell(5, 0.7, utf8_decode(strtoupper('Monto')), 1, '', 'C');
-        $this->Cell(5, 0.7, utf8_decode(strtoupper('Fecha')), 1, '', 'C');
-        $this->Ln();
-
-        $connection = \Yii::$app->db;
-        $sqlStatement = '
-         SELECT * from comision 
-        WHERE date(Fecha_Creado)  BETWEEN "' . $fechaIni . '" and "' . $fechaFin . '";
-        ';
-        $comando = $connection->createCommand($sqlStatement);
-        $resultado = $comando->query();
-
-        while ($row = $resultado->read()) {
-
-            $this->Cell(5, 0.7, utf8_decode(strtoupper($model->NombreUsuario($row['Codigo_usuario']))), 1, '', 'C');
-            $this->Cell(5, 0.7, utf8_decode(strtoupper($row['monto'])), 1, '', 'C');
-            $this->Cell(5, 0.7, utf8_decode(strtoupper($row['Fecha_Creado'])), 1, '', 'C');
-
-            $this->Ln();
-        }
-
-    }
+//    function Comision($fechaIni, $fechaFin)
+//    {
+//        $model = new \app\models\Usuario();
+//        $cliente = new \app\models\Cliente();
+//
+////      $this->Cell(Ancho , Alto , cadena , bordes , posición , alinear , fondo, URL )
+//        $this->SetFont('Arial', 'B', 18);
+//
+////      $this->Image('ruta de imagen', horizontal, vertical, ancho, alto);
+////        $this->Image(Yii::getAlias('@groupgygUrlReporte'), 1, 1, 1.5, 1.5);
+//        $this->Cell(78.5, 1, utf8_decode('Lista de Comisiones'), 0, 'C', 'C');
+//        $this->Ln(2);
+//
+//        $this->SetFont('Arial', 'B', 11);
+//        $this->SetFillColor(150, 54, 52); // establece el color del fondo de la celda.
+//        $this->SetDrawColor(150, 54, 52);     // establece el color del contorno de la celda.
+//        $this->SetTextColor(255, 255, 255);  // Establece el color del texto.
+//        $this->Cell(0.2, 0.2, utf8_decode(''), 0, 0, 'C', True);
+//        $this->Cell(75.1, 0.2, utf8_decode(''), 1, 0, 'C', True);
+//        $this->Cell(0.2, 0.2, utf8_decode(''), 0, 0, 'C', True);
+//        $this->Ln(0.6);
+//
+//
+//        $this->Cell(5, 0.7, utf8_decode(strtoupper('Datos del Usuario')), 1, '', 'C');
+//        $this->Cell(5, 0.7, utf8_decode(strtoupper('Monto')), 1, '', 'C');
+//        $this->Cell(5, 0.7, utf8_decode(strtoupper('Fecha')), 1, '', 'C');
+//        $this->Ln();
+//
+//        $connection = \Yii::$app->db;
+//        $sqlStatement = '
+//         SELECT * from comision
+//        WHERE date(Fecha_Creado)  BETWEEN "' . $fechaIni . '" and "' . $fechaFin . '";
+//        ';
+//        $comando = $connection->createCommand($sqlStatement);
+//        $resultado = $comando->query();
+//
+//        while ($row = $resultado->read()) {
+//
+//            $this->Cell(5, 0.7, utf8_decode(strtoupper($model->NombreUsuario($row['Codigo_usuario']))), 1, '', 'C');
+//            $this->Cell(5, 0.7, utf8_decode(strtoupper($row['monto'])), 1, '', 'C');
+//            $this->Cell(5, 0.7, utf8_decode(strtoupper($row['Fecha_Creado'])), 1, '', 'C');
+//
+//            $this->Ln();
+//        }
+//
+//    }
 }
 
 $pdf = new PDF('L', 'cm', array(22, 78.5));
 $Reporte = "Reporte_de_Venta.pdf";
 $pdf->AddPage();
 $pdf->Impresion($fechaIni, $fechaFin, $codigoClub, $codigoPasaporte, $codigoCliente, $CodigoVenta);
-$pdf->AddPage();
-$pdf->Comision($fechaIni, $fechaFin);
+//$pdf->AddPage();
+//$pdf->Comision($fechaIni, $fechaFin);
 $pdf->SetTitle("Reporte de Ventas");
 $pdf->SetAuthor("Rustica Club");
 $pdf->Output($Reporte, 'I');
